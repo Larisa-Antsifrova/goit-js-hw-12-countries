c890831bf263b372060b774238c1dc301140f1a4
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2020-12-22 16:37:00 +0200

    Added comments.

--- src/js/error-notification.js
+++ src/js/error-notification.js
@@ -1,3 +1,4 @@
+// Установлена 4-ая (предыдущая версия) PNotify, так как в 5-ой версии некорректно отображаются кнопки закрытия
 import PNotify from '../../node_modules/pnotify/dist/es/PNotify.js';
 import PNotifyButtons from '../../node_modules/pnotify/dist/es/PNotifyButtons.js';
 import '../../node_modules/pnotify/dist/PNotifyBrightTheme.css';
